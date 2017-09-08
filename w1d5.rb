fa905e3d4e424bbf45327fe74e415e328dd13b46
ralph salazar <ralph.slzr@gmail.com>
2017-09-07 23:15:53 -0400

    class Stack & class Queue

---
+++ w1d5.rb
@@ -0,0 +1,37 @@
+class Stack
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    @stack << el
+    el
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    @stack.dup
+  end
+end
+
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue << el
+    el
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def show
+    @queue.dup
+  end
+end
